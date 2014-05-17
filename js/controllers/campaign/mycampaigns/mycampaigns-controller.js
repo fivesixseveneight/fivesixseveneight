1138e038da1ab1c83d9fc273d73355cba14a225c
ken lin <kendrick.lin@alumni.utoronto.ca>
2014-05-17 17:05:50 -0400

    styled footer

--- js/controllers/campaign/mycampaigns/mycampaigns-controller.js
+++ js/controllers/campaign/mycampaigns/mycampaigns-controller.js
@@ -1,4 +1,4 @@
-define(['../module'], function (controllers) {
+define(['../../module'], function (controllers) {
     'use strict';
     controllers.controller('mycampaignsController', ['$scope','$rootScope', function ($scope, $rootScope) {
      	
--- js/controllers/campaign/requests/campaignrequests-controller.js
+++ js/controllers/campaign/requests/campaignrequests-controller.js
@@ -1,4 +1,4 @@
-define(['../module'], function (controllers) {
+define(['../../module'], function (controllers) {
     'use strict';
     controllers.controller('campaignrequestsController', ['$scope','$rootScope', function ($scope, $rootScope) {
      	
