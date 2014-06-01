a95044772ce819d1aae96d41ca118c5511ca610d
ken lin <kendrick.lin@alumni.utoronto.ca>
2014-06-01 03:56:06 -0400

    facebook friends api doesnt support counting the number of friends a user has, what a shame

--- api/lib/facebook/examples/example.php
+++ api/lib/facebook/examples/example.php
@@ -93,66 +93,18 @@ if ($user) {
   
 } else {
 // $statusUrl = $facebook->getLoginStatusUrl();
-  $loginUrl = $facebook->getLoginUrl();
+  $loginUrl = $facebook->getLoginUrl(array(
+  		"req_perms" => "read_stream,publish_stream"
+  ));
+  
+  
+  
 }
 
 // This call will always work since we are fetching public data.
 $naitik = $facebook->api('/naitik');
 
 ?>
-
-<?php
-  $app_id = '224679534393438';
-  $app_secret = 'e7ee425e809bd0ffb976d5968a3a5e8c';
-  $my_url = 'http://stage.fivesixseveneight.co/api/lib/facebook/examples/example.php';
-
-  $code = $_REQUEST["code"];
-
- // auth user
- if(empty($code)) {
-    $dialog_url = 'https://www.facebook.com/dialog/oauth?client_id=' 
-    . $app_id . '&redirect_uri=' . urlencode($my_url) ;
-    echo("<script>top.location.href='" . $dialog_url . "'</script>");
-  }
-
-  // get user access_token
-  $token_url = 'https://graph.facebook.com/oauth/access_token?client_id='
-    . $app_id . '&redirect_uri=' . urlencode($my_url) 
-    . '&client_secret=' . $app_secret 
-    . '&code=' . $code;
-
-  // response is of the format "access_token=AAAC..."
-  $access_token = substr(file_get_contents($token_url), 13);
-
-  // run fql query
-  $fql_query_url = 'https://graph.facebook.com/'
-    . 'fql?q=SELECT+uid2+FROM+friend+WHERE+uid1=me()'
-    . '&access_token=' . $access_token;
-  $fql_query_result = file_get_contents($fql_query_url);
-  $fql_query_obj = json_decode($fql_query_result, true);
-
-  // display results of fql query
-  echo '<pre>';
-  print_r("query results:");
-  print_r($fql_query_obj);
-  echo '</pre>';
-
-  // run fql multiquery
-  $fql_multiquery_url = 'https://graph.facebook.com/'
-    . 'fql?q={"all+friends":"SELECT+uid2+FROM+friend+WHERE+uid1=me()",'
-    . '"my+name":"SELECT+name+FROM+user+WHERE+uid=me()"}'
-    . '&access_token=' . $access_token;
-  $fql_multiquery_result = file_get_contents($fql_multiquery_url);
-  $fql_multiquery_obj = json_decode($fql_multiquery_result, true);
-
-  // display results of fql multiquery
-  echo '<pre>';
-  print_r("multi query results:");
-  print_r($fql_multiquery_obj);
-  echo '</pre>';
-?>
-
-
 
 
 <!doctype html>
